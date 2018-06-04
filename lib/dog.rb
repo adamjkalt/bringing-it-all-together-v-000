558598461efdb061fb16a21a19e6a998c2260ec6
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-04 18:10:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -91,7 +91,7 @@ def self.find_or_create_by(name:, breed:)
       WHERE name = ? AND breed = ?
       LIMIT 1
     SQL
-    test = DB[:conn].execute(sql, name, breed).map do |row|
+    dog_info = DB[:conn].execute(sql, name, breed).map do |row|
      self.new_from_db(row)
     end.first
     binding.pry
